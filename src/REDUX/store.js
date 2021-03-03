a6f285e5162793f044ea80e6706a034bcd4a8c18
Webioann <ioannn7777@gmail.com>
2021-03-03 13:07:16 +0200

    install all needed packges

---
+++ src/REDUX/store.js
@@ -0,0 +1,12 @@
+import {createStore,combineReducers,applyMiddleware} from "redux";
+import countReducer from './Reducers/countReducer'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+const rootReducer = combineReducers({
+    cash: countReducer,
+})
+
+const store = createStore(rootReducer,applyMiddleware(thunk,logger));
+
+export default store;
